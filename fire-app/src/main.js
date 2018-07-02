24de57c20182587bdbe93410cbedf713406fe2cd
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-07-02 15:10:42 -0700

    setup add and delete methods for photos on database

--- fire-app/src/main.js
+++ fire-app/src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
-Vue.config.productionTip = false
+Vue.use(VueFire)
+firebase.initializeApp({
+  projectId: "vuejs-test-e1a0c",
+  databaseURL: "https://vuejs-test-e1a0c.firebaseio.com"
+})
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +20,5 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+export const db = firebase.firestore()
